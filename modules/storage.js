5477a2e8e0601407da2279ef251b2606f8cde3c2
alejoevilches <alejoevilches@gmail.com>
2023-06-10 02:32:19 -0300

    items now can be deleted

--- modules/storage.js
+++ modules/storage.js
@@ -10,10 +10,16 @@ const createEvents=()=>{
 
 const handleLikeClick=(e)=>{
     let match=pokemons.find(pokemon=>pokemon.name==e.target.id);
-    favourites.some(pokemon=>pokemon.name==match.name) ? console.log("Ya existe") : favourites.push(match);
+    favourites.some(pokemon=>pokemon.name==match.name) ? deletePokemonFav(match) : favourites.push(match);
     console.log(favourites);
+    localStorage.setItem("pokemons", JSON.stringify(favourites));
 }
 
+const deletePokemonFav=(match)=>{
+    let index=favourites.indexOf(match);
+    favourites.splice(index,1);
+    console.log(favourites);
+}
 
 
 export default createEvents;

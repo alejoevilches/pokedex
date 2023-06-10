9e677b0bfafbd77259b8319e5fb073d1133dc3d8
alejoevilches <alejoevilches@gmail.com>
2023-06-10 02:41:42 -0300

    last work of the day. starting on showing favourite pokemons on screen

--- modules/storage.js
+++ modules/storage.js
@@ -1,5 +1,8 @@
 import pokemons from "/modules/cards.js";
 const favourites=[]
+const favsButton=document.querySelector("#favs");
+const cardContainer=document.querySelector(".container")
+console.log(favsButton);
 
 const createEvents=()=>{
     const likeButtons=document.querySelectorAll("box-icon");
@@ -11,15 +14,19 @@ const createEvents=()=>{
 const handleLikeClick=(e)=>{
     let match=pokemons.find(pokemon=>pokemon.name==e.target.id);
     favourites.some(pokemon=>pokemon.name==match.name) ? deletePokemonFav(match) : favourites.push(match);
-    console.log(favourites);
     localStorage.setItem("pokemons", JSON.stringify(favourites));
 }
 
 const deletePokemonFav=(match)=>{
     let index=favourites.indexOf(match);
     favourites.splice(index,1);
-    console.log(favourites);
+    localStorage.setItem("pokemons", JSON.stringify(favourites));
 }
 
+const showFavourites=()=>{
+    cardContainer.innerHTML="";
+}
+
+favsButton.addEventListener("click", showFavourites);
 
 export default createEvents;
